79f64ce7be788d083208efaaba7de2a1bafa72f2
Nestor Montanez <nestormontanez@Nestors-MacBook-Pro.local>
2023-03-07 22:39:41 -0500

    made clear storage feature for past searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,5 +134,20 @@ var renderCities = () => {
     }
 }
 
+$("#search-button").on("click", (event)=> {
+    event.preventDefault();
+    currentCity = $("#search-city").val();
+    getCurrentConditions(event);
+});
+
+$("#clear-stroage").on("click", (event) => {
+    localStorage.clear();
+    renderCities();
+});
+
+renderCities();
+
+getCurrentConditions();
+
 
 
